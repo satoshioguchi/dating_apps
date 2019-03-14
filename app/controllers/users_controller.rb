25395a0a3c18d54e7d0c1b5de48e9b25b9240f37
Satoshi Oguchi <sprint1091.to@gmail.com>
2019-03-14 14:53:49 +0000

    エラーパーシャルの作成、Userコントローラのcreateアクションの実装、signup_formの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,20 @@ class UsersController < ApplicationController
     @user = User.new
   end
   
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # 保存の成功をここで扱う。
+    else
+      render 'new'
+    end
+  end
+  
+   private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation, :birthdate,
+                                   :gender, :prefecture_id)
+    end
 end
