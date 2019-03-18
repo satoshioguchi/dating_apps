639d14aed301d0389396568c2ac75a4d07881cc6
Satoshi Oguchi <sprint1091.to@gmail.com>
2019-03-18 09:18:14 +0000

    users#updateの追加,newとeditフォームのパーシャルの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,20 @@ class UsersController < ApplicationController
     end
   end
   
-   private
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # 更新に成功した場合を扱う。
+    else
+      render 'edit'
+    end
+  end
+  
+  private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
