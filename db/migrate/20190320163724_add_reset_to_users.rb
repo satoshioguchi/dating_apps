f5db848336966e70e61ce9f746fb0093e03ed57f
Satoshi Oguchi <sprint1091.to@gmail.com>
2019-03-21 07:22:39 +0000

    Relationshipモデルの作成、リレーションによるfollowing,followersメソッドの追加

--- db/migrate/20190320163724_add_reset_to_users.rb
+++ db/migrate/20190320163724_add_reset_to_users.rb
@@ -1,6 +1,13 @@
-class AddResetToUsers < ActiveRecord::Migration[5.1]
+class CreateRelationships < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :reset_digest, :string
-    add_column :users, :reset_sent_at, :datetime
+    create_table :relationships do |t|
+      t.integer :follower_id
+      t.integer :followed_id
+
+      t.timestamps
+    end
+    add_index :relationships, :follower_id
+    add_index :relationships, :followed_id
+    add_index :relationships, [:follower_id, :followed_id], unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190320163724) do
+ActiveRecord::Schema.define(version: 20190321070316) do
 
   create_table "prefectures", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
